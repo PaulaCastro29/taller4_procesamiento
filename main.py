c5f42bbebb7434076387f176db7186112a7ed928
PaulaCastro29 <88325661+PaulaCastro29@users.noreply.github.com>
2021-09-29 23:22:02 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from hough import Hough
 from orientation_methods import gradient_map
 import cv2
 import random
+import poly_point_insect as bot
 
 class lines:
 
@@ -20,7 +21,7 @@ class lines:
         if NPar==0 and self.N>200:
             self.N=self.N
         else:
-            print ("Error! el numero ingresado no es un numero par o no es mayor a 100")
+            print ("Error! el numero ingresado no es un numero par o no es mayor a 200")
 
     # Método que contrulle una imagen en fondo color cian y un cuadrilatero en color magenta
     def generate(self):
@@ -29,14 +30,21 @@ class lines:
 
         x = random.randint(self.N / 2, self.N)
 
-        # Dibujo "cuadrilatero"
+        # Dibujo rectangulo aleatorio
         cv2.rectangle(imagen, (x, 100), (100, x), (255, 0, 255), -1)
 
-        cv2.imshow('imagen', imagen)
+        #Para probar los metodos con un poligono quitar comentario de las siguientes lineas
+        '''
+        pts = np.array([[25, 70], [25, 160],
+                        [110, 200], [200, 160],
+                        [200, 70], [110, 20]], np.int32)
+        pts = pts.reshape((-1, 1, 2))
+        cv2.polylines(imagen, [pts], True, (255, 0, 255))
+        '''
+        cv2.imshow('imagen generada', imagen)
         cv2.imwrite('example2.png', imagen)
-        cv2.waitKey(0)
 
-    #Método que detecta las lineas de una figura
+    #Método que detecta las lineas de una figura y sus intersecciones
     def DetectCorners(self):
         Standard = 1
         Direct = 2
@@ -84,21 +92,57 @@ class lines:
             y2 = int(round(y0 - cols * a))
 
             if np.abs(theta_) < 80:
-                image_draw = cv2.line(image_draw, (x1, y1), (x2, y2), [0, 255, 255], thickness=2)
+                image_draw = cv2.line(image_draw, (x1, y1), (x2, y2), [2550, 0, 255], thickness=2)
             elif np.abs(theta_) > 100:
                 image_draw = cv2.line(image_draw, (x1, y1), (x2, y2), [255, 0, 255], thickness=2)
             else:
                 if theta_ > 0:
-                    image_draw = cv2.line(image_draw, (x1, y1), (x2, y2), [0, 255, 0], thickness=2)
+                    image_draw = cv2.line(image_draw, (x1, y1), (x2, y2), [255, 0, 255], thickness=2)
                 else:
-                    image_draw = cv2.line(image_draw, (x1, y1), (x2, y2), [0, 0, 255], thickness=2)
+                    image_draw = cv2.line(image_draw, (x1, y1), (x2, y2), [255, 0, 255], thickness=2)
 
         cv2.imshow("frame", bw_edges)
-        cv2.imshow("lines", image_draw)
+        cv2.imwrite("lines.png", image_draw)
+
+        #El codigo que se emplea de aqui en adelante permite encontrar los puntos intersectos usando la funcion bot tomado de: https://github.com/ideasman42/isect_segments-bentley_ottmann/blob/master/poly_point_isect.py
+        gray = cv2.cvtColor(image_draw, cv2.COLOR_BGR2GRAY)
+        kernel_size = 5
+        blur_gray = cv2.GaussianBlur(gray, (kernel_size, kernel_size), 0)
+
+        low_threshold = 50
+        high_threshold = 150
+        edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
+
+        rho = 1
+        theta = np.pi / 180
+        threshold = 15
+        min_line_length = 50
+        max_line_gap = 20
+        line_image = np.copy(image_draw) * 0
+
+        lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]),
+                    min_line_length, max_line_gap)
+        points = []
+        for line in lines:
+            for x1, y1, x2, y2 in line:
+                points.append(((x1 + 0.0, y1 + 0.0), (x2 + 0.0, y2 + 0.0)))
+                cv2.line(line_image, (x1, y1), (x2, y2), (0, 255, 255), 5)
+
+        lines_edges = cv2.addWeighted(image_draw, 0.8, image_draw, 0, 0)
+        intersections = bot.isect_segments(points)
+        print('Los puntos de interseccion de las lineas estan en:',intersections)
+
+        for inter in intersections:
+            a, b = inter
+            for i in range(10):
+                for j in range(10):
+                    lines_edges[int(b) + i, int(a) + j] = [0, 255, 255]
+
+        cv2.imshow('Intersecciones', lines_edges)
         cv2.waitKey(0)
+
 
 if __name__ == '__main__':
     clase = lines()
     clase.generate()
     clase.DetectCorners()
-
